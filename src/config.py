c34f14801e9db226ff264523b5a4a2380bf3c4e1
omrifahn <omrifahn@gmail.com>
2025-01-25 22:42:37 +0200

    Update dataset path to 500_question_answers

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 import os
 from credentials import HUGGINGFACE_TOKEN, OPENAI_API_KEY
 
-DATASET_PATH = "./data/data.json"
+DATASET_PATH = "./data/500_question_answers"
 OUTPUT_DIR = "./output"
 
 LOCAL_MODEL_NAME = "meta-llama/Llama-2-7b-hf"
--- src/evaluator.py
+++ src/evaluator.py
@@ -9,7 +9,7 @@ from openai import OpenAI
 from pydantic import BaseModel
 
 from local_llm import load_local_model, get_local_llm_answer
-from config import OUTPUT_DIR, LOCAL_MODEL_NAME, DATASET_PATH, OPENAI_API_KEY
+from config import OUTPUT_DIR, LOCAL_MODEL_NAME, DATASET_PATH, OPENAI_API_KEY, DATA_LIMIT
 
 client = OpenAI(api_key=OPENAI_API_KEY)
 
