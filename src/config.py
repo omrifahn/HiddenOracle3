665f19b68b4c78a07a1fdb58bbe793e41dbdcc89
omrifahn <omrifahn@gmail.com>
2025-01-25 22:24:25 +0200

    api key correct import

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 import os
+from credentials import HUGGINGFACE_TOKEN, OPENAI_API_KEY
 
 DATASET_PATH = "./data/data.json"
 OUTPUT_DIR = "./output"
@@ -6,7 +7,3 @@ OUTPUT_DIR = "./output"
 LOCAL_MODEL_NAME = "meta-llama/Llama-2-7b-hf"
 LOCAL_MODEL_DIR = "./models"  # Directory to save/load the local model
 USE_LOCAL_MODEL_STORAGE = True  # Flag to control local model storage
-
-# Load API keys from environment variables for security
-HUGGINGFACE_TOKEN = os.environ.get("HUGGINGFACE_TOKEN")
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
