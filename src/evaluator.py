60f7f7677b3d2db0c691d426e3e0694a5b45c106
omrifahn <omrifahn@gmail.com>
2025-02-03 21:14:05 +0200

    new syntax comment for GPT

--- src/evaluator.py
+++ src/evaluator.py
@@ -5,13 +5,13 @@ from typing import List, Dict, Any
 
 import time
 
-from openai import OpenAI
+from openai import OpenAI # new sytax to import open api versions 1.0.0+
 from pydantic import BaseModel
 
 from local_llm import load_local_model, get_local_llm_answer
 from config import OUTPUT_DIR, LOCAL_MODEL_NAME, DATASET_PATH, OPENAI_API_KEY, DATA_LIMIT
 
-client = OpenAI(api_key=OPENAI_API_KEY)
+client = OpenAI(api_key=OPENAI_API_KEY) # new sytax for open api versions 1.0.0+
 
 
 # 1. Load dataset
