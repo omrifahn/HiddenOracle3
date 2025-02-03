6efa9c7ef33e50e4c18089a4ed9adb6708719142
omrifahn <omrifahn@gmail.com>
2025-02-03 21:41:44 +0200

    fix file names

--- src/config.py
+++ src/config.py
@@ -4,11 +4,11 @@ from credentials import (
     OPENAI_API_KEY,
 )  # we want to import those tokens through config.py so credentials.py is not exposed to the public
 
-DATASET_PATH = "./data/500_question_answers"
+DATASET_PATH = "./data/500_question_answers.json"
 OUTPUT_DIR = "./output"
 
 LOCAL_MODEL_NAME = "meta-llama/Llama-2-7b-hf"
-LOCAL_MODEL_DIR = "./src/models"  # Directory to save/load the local model
+LOCAL_MODEL_DIR = "../models"  # Directory to save/load the local model
 USE_LOCAL_MODEL_STORAGE = True  # Flag to control local model storage
 
 DATA_LIMIT = 3  # Maximum number of dataset items to process
